7c3be8ec47429213bf3a38eb830c21de7999a357
felixzhu1989 <zhu-hongfeng@hotmail.com>
2022-06-16 09:08:00 +0800

    UiDesign1

--- WpfMvvmDataGrid/obj/Debug/net6.0-windows/Views/StudentView.g.i.cs
+++ WpfMvvmDataGrid/obj/Debug/net6.0-windows/Views/StudentView.g.i.cs
@@ -63,13 +63,13 @@ namespace WpfMvvmDataGrid.Views {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfMvvmDataGrid;component/views/studentview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfMvvmDataGrid;V1.0.0.0;component/views/studentview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\StudentView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -79,7 +79,7 @@ namespace WpfMvvmDataGrid.Views {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
--- WpfUiDemo.FreeUid1/MainWindow.xaml.cs
+++ WpfUiDemo.FreeUid1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,30 @@ namespace WpfUiDemo.FreeUid1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<User> Users { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            Users = new ObservableCollection<User>()
+            {
+                new User(){ImagePath="/user.jpg",UserName="Felix",Country="China",LoginTime="30 Mins"},
+                new User(){ImagePath="/user.jpg",UserName="Felix2",Country="China",LoginTime="3 Days"},
+                new User(){ImagePath="/user.jpg",UserName="Felix3",Country="China",LoginTime="30 Weeks"},
+                new User(){ImagePath="/user.jpg",UserName="Felix4",Country="China",LoginTime="30 years"},
+                new User(){ImagePath="/user.jpg",UserName="Felix5",Country="China",LoginTime="50 Mins"},
+                new User(){ImagePath="/user.jpg",UserName="Felix6",Country="China",LoginTime="60 Mins"},
+            };
+            this.UserList.ItemsSource=Users;
         }
+        
+
+
+    }
+   public class User
+    {
+        public string ImagePath { get; set; }
+        public string UserName { get; set; }
+        public string Country { get; set; }
+        public string LoginTime { get; set; }
     }
 }
