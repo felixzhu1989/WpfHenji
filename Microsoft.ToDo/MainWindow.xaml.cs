f6506ea95c5f107ec539f0a4415b5bdf65aa5a84
朱宏峰 <zhu-hongfeng@hotmail.com>
2022-06-19 20:10:47 +0800

    Micorsofr.ToDo

---
+++ Microsoft.ToDo/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Resources;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+using System.Collections;
+using System.Windows.Baml2006;
+using System.IO;
+using System.Windows.Markup;
+using System.Diagnostics;
+using System.Reflection;
+using System.Drawing;
+
+namespace Microsoft.ToDo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DirectoryInfo folder = new DirectoryInfo(@"C:\Program Files (x86)\HuyaLive\HuyaCient\Net45");
+            foreach (var file in folder.GetFiles("*.dll"))
+            {
+                try
+                {
+                    //获取所有的图片资源
+                    var assembly = Assembly.LoadFile(file.FullName);
+                    var resourcesName = $"{assembly.GetType().Assembly.GetName().Name}.g";
+                    var manager = new ResourceManager(resourcesName, assembly.GetType().Assembly);
+                    var resourceSet = manager.GetResourceSet(CultureInfo.CurrentCulture, true, true);
+                    var dictonaryEntries = resourceSet.OfType<DictionaryEntry>().ToList();
+                    dictonaryEntries.ForEach(entry =>
+                    {
+                        if (entry.Key.ToString().Contains("png"))
+                        {
+                            Bitmap bitmap = new Bitmap((Stream)entry.Value);
+                            string saveFilePath = @$"{AppDomain.CurrentDomain.BaseDirectory}Image\{Guid.NewGuid().ToString()}.png";
+                            bitmap.Save(saveFilePath);
+                        }
+                    //Baml2006Reader reader = new Baml2006Reader((Stream)entry.Value);
+                    //var win = XamlReader.Load(reader) as Window;
+                    //Debug.Print(win.Name);
+                });
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ Microsoft.ToDo/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.ToDo.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ToDo.ViewModels;
+
+public class MainViewModel
+{
+    private ObservableCollection<MenuItem> menuItems;
+    public ObservableCollection<MenuItem> MenuItems
+    {
+        get { return menuItems; }
+        set { menuItems = value; }
+    }
+    public MainViewModel()
+    {
+        MenuItems = new ObservableCollection<MenuItem>
+        {
+            new MenuItem(){Icon="\xe64c",BackColor="Green",Name="我的一天",Count=1},
+            new MenuItem(){Icon="\xe600",BackColor="Red",Name="重要",Count=3},
+            new MenuItem(){Icon="\xe602",BackColor="DodgerBlue",Name="已计划日程",Count=4},
+            new MenuItem(){Icon="\xe633",BackColor="Orange",Name="分配给你",Count=8},
+            new MenuItem(){Icon="\xe60f",BackColor="LightSeaGreen",Name="任务",Count=2},
+        };
+    }
+}
